0cbe9f2f18c5e6d43a59a177a1ea9e5b8ab58af5
hzy <i@huzheyang.com>
2019-06-15 19:15:48 +0800

    +x

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 touch /config/aria2.session
 envsubst < config.template > aria2.conf
 cp -n aria2.conf /config/aria2.conf
-aria2c --conf-path=/config/aria2.conf
+chmod +x aria2c
+./aria2c --conf-path=/config/aria2.conf
